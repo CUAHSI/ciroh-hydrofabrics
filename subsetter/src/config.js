425fbe558bffa01d7cd9bba94f1b041768e30bcb
zahraa222 <zahraaaa@umich.edu>
2026-05-12 12:15:58 -0400

    set up directories, begin splitting js logic

---
+++ subsetter/src/config.js
@@ -0,0 +1,16 @@
+    const S3_PARQUET = 'https://s3.hydroshare.org/sblack/4219997533bf46a1893e9ba0232403eb/data/contents/parquet';
+    const S3_MAP = 'https://s3.hydroshare.org/sblack/4219997533bf46a1893e9ba0232403eb/data/contents/map';
+    const NETWORK_GRAPH_URL = `${S3_PARQUET}/network_graph.json`;
+
+    const PARQUET_URLS = {
+      'divides':                `${S3_PARQUET}/divides.parquet`,
+      'divide-attributes':      `${S3_PARQUET}/divide-attributes.parquet`,
+      'flowpaths':              `${S3_PARQUET}/flowpaths.parquet`,
+      'flowpath-attributes':    `${S3_PARQUET}/flowpath-attributes.parquet`,
+      'flowpath-attributes-ml': `${S3_PARQUET}/flowpath-attributes-ml.parquet`,
+      'hydrolocations':         `${S3_PARQUET}/hydrolocations.parquet`,
+      'nexus':                  `${S3_PARQUET}/nexus.parquet`,
+      'pois':                   `${S3_PARQUET}/pois.parquet`,
+      'lakes':                  `${S3_PARQUET}/lakes.parquet`,
+      'network':                `${S3_PARQUET}/network.parquet`,
+    };
